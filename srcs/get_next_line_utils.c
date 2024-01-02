35b085bc82e27165c0dbaf903a1bd376df2e036a
goldishmthfckz <124591796+goldishmthfckz@users.noreply.github.com>
2024-01-02 17:02:08 +0100

    Update get_next_line_utils.c

--- srcs/get_next_line_utils.c
+++ srcs/get_next_line_utils.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../incl/get_next_line.h"
 
 int	ft_strlen(char *str)
 {
